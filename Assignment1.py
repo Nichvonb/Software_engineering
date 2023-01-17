e8423eddee06da1c38eeecdf64f5ec9645c077e0
Nicholas <nvb@kth.se>
2023-01-17 13:11:51 +0100

    Added Features

--- Assignment1.py
+++ Assignment1.py
@@ -1,5 +1,15 @@
 import numpy as np
 #make square numpy matrix of given size
 def makeSquareMatrix(size):
-    matrix = np.zeros((size,size))
-    return matrix
+    matrix_k = np.zeros((size,size))
+    return matrix_k
+
+#make a list of all the possible moves
+def makeMoveList(size):
+    moveList = []
+    for i in range(size):
+        for j in range(size):
+            moveList.append((i,j))
+    return moveList
+
+
